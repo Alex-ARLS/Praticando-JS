2d7a1119d3a4d856715772f3ca1348c4adbcb447
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-12 19:32:19 -0300

    master

--- aula 4/comentarios.js
+++ aula 4/comentarios.js
@@ -1,2 +1,4 @@
 // e escrever um comentario. barra barra.
 console.log('ola mundo!')
+/* aqui pode ter um texto gigantesco  com varias linhas
+de codigo e tudo isso sera ignorado pelo motor do JS */
