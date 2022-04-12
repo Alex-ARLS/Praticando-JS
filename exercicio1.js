739722bd1b531b1dc6352178a13ed5c95985a46c
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-12 19:25:19 -0300

    master

---
+++ exercicio1.js
@@ -0,0 +1,3 @@
+console.log(`Meu nome é "Alex Araujo". estou aprendendo JavaScript às neste exato momento, às`, 10, `da noite`);   
+console.log('Meu nome é Alex Araujo. estou aprendendo JavaScript às neste exato momento às', 12, 'da noite');
+console.log('meu', 'nome', 'is', 'alex', 'estou','aprendendo', 'javascript', 'sao', 15, 'da noite'); 
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("ola, mundo"); // texto = string
+console.log('ola, mundo'); //ponto e virgula is opcional no JS 
+console.log(`'luis' "eduardo"`); // crase pega ambas as aspas
+
+console.log(1,2,3,4,5,6,7,8,9,0, 15.86234234, 'luiz otavio'); // number = numero
