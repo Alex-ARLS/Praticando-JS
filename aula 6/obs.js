293f1f74b9bcd09e411e97ac9d4560e8acf94ae0
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-12 21:14:09 -0300

    master

---
+++ aula 6/obs.js
@@ -0,0 +1,8 @@
+// nao podemos criar variaveis com palavras reservadas
+// variaveis precisam de nomes significativos
+// n pode comecar o nome de uma variavel com um numero. e letras minusculas se possivel
+// n pode conter espacos ou tracos se nao nao funciona
+// utilizar o camelCase(nomeCliente) primeira letra minuscula com as proximas sendo maiusculas
+// case-sensitive minuscula e maiscula faz diferenca
+//nao podemos redeclarar variaveis com let
+// nao utilizar var, utilizar let
