2673ee94cd368d1f75634e7f481cc9ff74e263db
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-13 11:15:30 -0300

    maste

--- aula7/const e let.js
+++ aula7/const e let.js
@@ -6,8 +6,8 @@ const resultadoTriplicado = resultadoDuplicado * 3;
 let resultadoQuadruplicado = resultadoTriplicado + 200;
 resultadoQuadruplicado = resultadoQuadruplicado + 600; //a let quando alterada perde o valor do ultimo numero diferentementa da const q vc pode usar a qq momento.
 console.log (resultadoQuadruplicado);
-console.log(resultadoDuplicado + resultadoDuplicado);
-console.log(resultadoDuplicado + resultadoDuplicado + resultado);
+console.log (resultadoDuplicado + resultadoDuplicado);
+console.log (resultadoDuplicado + resultadoDuplicado + resultado);
 console.log (resultado * resultadoTriplicado);
 console.log ( resultado / resultadoTriplicado)
 
