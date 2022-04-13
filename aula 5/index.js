68ffd0b68fd56d6b11dc7c050c2b17eda672ad67
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-12 21:14:00 -0300

    master

--- aula 5/index.js
+++ aula 5/index.js
@@ -1,1 +1,5 @@
+
 console.log('ola mundo, pega essa manga');
+console.log('estas strings com log serao vistas na option inspecionar');
+//alert('ola mundo'); alerta n esta definido no node.js apenas no console do navegador um exemplo de seperacao do node e do console do baco do front
+
