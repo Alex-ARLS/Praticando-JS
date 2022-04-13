32dafe223d1b2185f4950e949ec25976bf9d20c1
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-13 20:26:52 -0300

    master

---
+++ aula8/exercicio 8 let const.js
@@ -0,0 +1,14 @@
+/* Andre Otavio Miranda tem 30 anos, pesa 84 kg
+tem 1.8 de altura e seu IMC is 25.925925925925924
+andre otavio nasceu em 1980
+*/
+const nome = 'Andre Otavio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const alturaEmM = 1.80;
+let imc = (peso / (alturaEmM * alturaEmM));
+console.log(imc);
+
+let anoNascimento= 2022 - idade;
+console.log (anoNascimento);
