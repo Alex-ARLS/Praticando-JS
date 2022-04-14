42e79a96a6d7e7f36a8f68cfea07917e5709f28d
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-14 10:51:12 -0300

    master

--- aula 10/index.js
+++ aula 10/index.js
@@ -1,8 +1,10 @@
-const nome = 'luiz';//string
-const nome1 = "luiz";//string
-const nome2 = `luiz`;//string
-const num = 10;//number
-const num1 = 10.52;//number
+// string, number, undefined, null, boolean esses sao os tipos de dados primitivos em JS
+const nome = 'luiz'; //string
+const nome1 = "luiz"; //string
+const nome2 = `luiz`; //string
+const num = 10; //number
+const num1 = 10.52; //number
 let nomeAluno;  // undefined -> nao aponta pra local nenhum na memoria
 let sobrenomeAluno = null;  //nulo -> nao aponta pra local nenhum na memoria
 const aprovado = false; //Boolean = true, false (logico)
+
