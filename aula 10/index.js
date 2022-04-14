0d40e2311cf76e4515483284320c5088688b58bc
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-14 09:20:08 -0300

    master

---
+++ aula 10/index.js
@@ -0,0 +1,8 @@
+const nome = 'luiz';//string
+const nome1 = "luiz";//string
+const nome2 = `luiz`;//string
+const num = 10;//number
+const num1 = 10.52;//number
+let nomeAluno;  // undefined -> nao aponta pra local nenhum na memoria
+let sobrenomeAluno = null;  //nulo -> nao aponta pra local nenhum na memoria
+const aprovado = false; //Boolean = true, false (logico)
