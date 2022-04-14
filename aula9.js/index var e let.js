02b5b537ff4ad76ce16c024f89b18598807db1d1
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-14 08:42:17 -0300

    master

---
+++ aula9.js/index var e let.js
@@ -0,0 +1,9 @@
+let nome = 'Luiz'; //ECMANScript is uma padronizacao do js conhecida por (ES6)
+nome = 'otavio'; // nao se pode redeclarar com let logo em seguida
+
+console.log(nome);
+
+
+//var is um scopo global, a variavel vai ficar rondando em todo o codigo
+//let is um scopo por funcao, fecha funcao variavel nao existe nas proximas
+//const um scopo tbm igual o let porem se diferencia da nao alteracao do tipo de objeto apos a declaracao
