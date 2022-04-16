2e1cc50bb2bf0f5f7293c6c7dc96846f2e5f8bcc
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-15 23:21:55 -0300

    master

---
+++ aula 11/operadores aritimetricos.js
@@ -0,0 +1,11 @@
+const num1 = 80;
+const num2 = 400;
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num1 * num2);
+console.log(num2 / num1);
+
+
+/*aritimetricos, adicao, sub, mult ou concatenar, - e /
+concatenar is unir os dois valores
+*/
