663e1c357e0190f72ca848e75ee37f7503b62c9a
Alex-ARLS <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-16 17:07:39 -0300

    master

--- aula 11/const contador = 2;.js
+++ aula 11/const contador = 2;.js
@@ -4,4 +4,5 @@ let contador = 9;
 ++contador;
 ++contador;
 console.log(contador);
-console.log(++contador);
+console.log(++contador); // incremento= ++ e decremento = --
+// pre decremento() e pos decremento()
