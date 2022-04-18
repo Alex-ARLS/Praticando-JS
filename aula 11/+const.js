1e787b9e6d4e3e1c150883e953e656778e56caf7
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-18 08:21:26 -0300

    master

---
+++ aula 11/+const.js
@@ -0,0 +1,4 @@
+const num1 = 50;
+const num2 = '8.6890' ; // parseInt para inteiros, parse float para flutuantes. isso serve para converter strings para numbers, Number vai tentar saber qual e o tipo de numero
+console.log(num1 + num2);
+console.log(typeof num2);
