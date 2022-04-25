a9b725dc9ee7cdbfd182e019097ead817115519e
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-04-24 22:56:38 -0300

    master

--- aula12/script.js
+++ aula12/script.js
@@ -1,1 +1,8 @@
-alert('Bem-vindo ao digitrapid');
+let num1 = prompt('digite o primeiro numero da soma');
+let num2 = prompt('digite o segundo numero da soma');
+num1 = Number(num1);
+num2 = Number(num2);
+const resultado = num1 + num2;
+
+alert('o resultado foi: '+ resultado);
+12
