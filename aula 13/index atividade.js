c1bdf90a97f0d4d1ccd673b0a9f0805edacb7a8a
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-06 04:08:27 -0300

    master

---
+++ aula 13/index atividade.js
@@ -0,0 +1,9 @@
+let varA = 'A'; // B
+let varB = 'B'; // C
+let varC = 'C'; // A
+
+
+[varA, varB, varC] = [varB, varC, varA] //mais atual pra segurar o valor da variavel primaria
+console.log(varA, varB, varC);
+
+//criando const temp na variavel const serve pra salvar o valor da variavel, meio que reservar ela.
