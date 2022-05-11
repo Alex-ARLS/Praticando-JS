e52be8228ebef6ae95bc4672f40f69a19d37219d
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-11 04:24:23 -0300

    master

--- aula 14/index.js
+++ aula 14/index.js
@@ -4,6 +4,17 @@ let num2 = 0.1;
 num1 += num2; // num1 = num1 + num2
 num1 += num2;
 num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2; // 2.0 numero inteiro o que a tofixed number corrige, para nao haver imprecisao, muito importante para software bancarios
+num1 = Number(num1.toFixed(2)); //isso serve pra deixar apenas 2 decimais.
 console.log(num1);
 console.log(Number.isInteger(num1));
 
@@ -11,11 +22,17 @@ console.log(Number.isInteger(num1));
 
 
 //console.log(num1.toString(); + num2); // concatenarpq transformei em uma string
+
 //num1 = num1.toString(); // .tostring serve para pegar o number e transformar em string
+
 //console.log(num1.toString(2)); // dentro dos parenteses vc pode colocar a representacao binaria daquele number zeros e um(2)
+
 //console.log(num1.toFixed(2)); // com o tofixed vc pode colocar o numero das casas decimais que vc quer
+
 //console.log(Number.isInteger(num1)); //Number.isInteger e responsavel por verificar se e inteiro ou nao
+
 //let temp = num1 * 'alo'; //NaN Not a Number.isNaN (xxx) pra saber quando n e um numero
+
 //console.log(Number.isNaN(temp));
 
 
