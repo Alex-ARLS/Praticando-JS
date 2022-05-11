201471e3f6ac7bd55023a857cb0da84d54861540
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-11 03:32:58 -0300

    master

---
+++ aula 14/index.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 2.4;
+
+//console.log(num1.toString(); + num2); // concatenarpq transformei em uma string
+//num1 = num1.toString(); // .tostring serve para pegar o number e transformar em string
+//console.log(num1.toString(2)); // dentro dos parenteses vc pode colocar a representacao binaria daquele number zeros e um(2)
+//console.log(num1.toFixed(2)); // com o tofixed vc pode colocar o numero das casas decimais que vc quer
+//console.log(Number.isInteger(num1)); //Number.isInteger e responsavel por verificar se e inteiro ou nao
+let temp = num1 * 'alo'; //NaN Not a Number.isNaN (xxx) pra saber quando n e um numero
+console.log(Number.isNaN(temp));
+
