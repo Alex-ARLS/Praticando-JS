5b4613dd299ec5afdaa89e0b50f736f6733284bc
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-12 23:19:35 -0300

    master

--- aula 15/indexmath.js
+++ aula 15/indexmath.js
@@ -1,9 +1,10 @@
-let num1 = 5678567859;
+//let num1 = 5678567859;
 //console.log(num1 ** (1/2));// pra pegar a raiz quadrada ou ** (0.5)
-console.log(num1 ** 0.5);// pegar raiz quadrada.
+//console.log(num1 ** 0.5);// pegar raiz quadrada.
 //let num2 = Math.ceil(num1); //arredondado para baixo floor, pra cima ceil
 //let num2 = Math.round(num1); // vai arredonda pra o mais proximo(na metade arredonda pra cima)
 //console.log(Math.max(1, 2, 4 , 5));// max vai pegar o maior numero da sequencia, e min o menor
 //console.log(Math.random()); // random number entre 0 e 1.
 //const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
 //console.log(Math.pow(2, 45)); // 2 elevado a 45 ou que [e mais facil (2 ** 10)] dois **
+console.log(100 / 0.0000000000000000000000000000000); // infinity é true
