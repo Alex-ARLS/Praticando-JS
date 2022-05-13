1bc222bf3269e2d6ff7b8da5dd14599deeca6d95
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-12 23:08:11 -0300

    master

---
+++ aula 15/indexmath.js
@@ -0,0 +1,7 @@
+let num1 = 3.5;
+//let num2 = Math.ceil(num1); //arredondado para baixo floor, pra cima ceil
+//let num2 = Math.round(num1); // vai arredonda pra o mais proximo(na metade arredonda pra cima)
+//console.log(Math.max(1, 2, 4 , 5));// max vai pegar o maior numero da sequencia, e min o menor
+//console.log(Math.random()); // random number entre 0 e 1.
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
