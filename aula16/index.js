6e38875fe24f852d370ee706be82259e344913e8
Alex Araújo <94236289+Alex-ARLS@users.noreply.github.com>
2022-05-18 20:31:36 -0300

    master

--- aula16/index.js
+++ aula16/index.js
@@ -1,1 +1,6 @@
 const numero = prompt('digite um numero:');
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+numeroTitulo.innerHTML = numero// alteramos o elemento do html span
+texto.innerHTML = `<p> Seu numero - 2 é `;
